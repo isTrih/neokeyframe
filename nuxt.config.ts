cea38f1e2492f60ed52f46e86f13baa791a62317
isTrih <huahaohui@gmail.com>
2025-01-04 11:38:56 +0800

    Using the ESLint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+// 运行或构建项目时，能够自动执行 ESLint 代码检查和修复的插件
+import eslint from 'vite-plugin-eslint'
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
@@ -12,4 +15,48 @@ export default defineNuxtConfig({
   modules: [
     '@unocss/nuxt',
   ],
+  // vite 构建工具配置
+  vite: {
+    // vite 插件配置
+    plugins: [
+      // 运行或构建项目时，能够自动执行 ESLint 代码检查和修复的插件
+      eslint({
+        // 缓存现在可以正确识别文件更改，以减少执行时间
+        cache: true,
+        // 开启自动修复
+        fix: true,
+        // 打印警告信息
+        emitWarning: true,
+        // 发生警告不停止构建继续执行
+        failOnWarning: false,
+        // 打印错误信息
+        emitError: true,
+        // 发生错误停止构建
+        failOnError: true,
+        // 指定排除检查的文件和目录
+        exclude: [
+          '**/node_modules/**',
+          '**/dist/**',
+          '**/public/**',
+          '**/.nuxt/**',
+          '**/.output/**',
+          '**/.nitro/**',
+          '**/.data/**',
+        ],
+        // 指定要执行检查的文件和目录
+        include: [
+          './**/*.ts',
+          './**/*.tsx',
+          './**/*.js',
+          './**/*.jsx',
+          './**/*.vue',
+          './*.ts',
+          './*.tsx',
+          './*.js',
+          './*.jsx',
+          './*.vue',
+        ],
+      }),
+    ],
+  },
 })
