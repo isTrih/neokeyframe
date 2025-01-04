cf70af8354fe279c95e49e3f0094ae417b6c3f69
isTrih <huahaohui@gmail.com>
2025-01-04 11:23:14 +0800

    Add the TypeScript support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  // typescript 配置
+  typescript: {
+    // 启用项目启动运行或构建时自动类型检查
+    typeCheck: true,
+    // 开启严格模式
+    strict: true
+  },
   modules: [
     '@unocss/nuxt',
   ],
