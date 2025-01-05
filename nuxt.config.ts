ade28a5623cf3ed7c5097f5883afdd4f3a6e93ae
isTrih <huahaohui@gmail.com>
2025-01-05 12:21:25 +0800

    添加Native UI需导入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// 引入unplugin-auto-import插件，用于naive-ui组件自动导入
+import AutoImport from 'unplugin-auto-import/vite'
+// 引入unplugin-vue-components插件，用于naive-ui组件自动导入
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 
 // 运行或构建项目时，能够自动执行 ESLint 代码检查和修复的插件
 import eslint from 'vite-plugin-eslint'
@@ -56,6 +61,22 @@ export default defineNuxtConfig({
           './*.vue',
         ],
       }),
+      // 引入unplugin-auto-import插件，用于naive-ui组件自动导入
+      AutoImport({
+        imports: [
+          {
+            'naive-ui': [
+              'useDialog',
+              'useMessage',
+              'useNotification',
+              'useLoadingBar'
+            ]
+          }
+        ]
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()]
+      })
     ],
   },
 })
