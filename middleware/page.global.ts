3175bc3c7481de9135839fba8c6a0dcf652b0304
TRIH <istrih@icloud.com>
2025-01-06 15:55:22 +0800

    WIP 默认PC端|主页进度35%

--- middleware/page.global.ts
+++ middleware/page.global.ts
@@ -1,7 +1,6 @@
 export default defineNuxtRouteMiddleware((to) => {
   // console.log(device.desktop());
   if (to.path === "/about" || to.path === "/about/") {
-    console.log("关于");
     setPageLayout("doc");
   }
 });
