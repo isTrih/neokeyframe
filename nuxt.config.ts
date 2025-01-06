c9007f84fc27cd04c2b6a7b217c63f7c2f56443c
TRIH <istrih@icloud.com>
2025-01-07 00:51:50 +0800

    WIP 【修复】ESM兼容问题|默认PC端|主页进度39%

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,9 @@ import Components from "unplugin-vue-components/vite";
 
 // 运行或构建项目时，能够自动执行 ESLint 代码检查和修复的插件
 export default defineNuxtConfig({
+  build: {
+    transpile: ["vueuc"],
+  },
   nitro: {
     compressPublicAssets: true,        // 启动压缩
   },
