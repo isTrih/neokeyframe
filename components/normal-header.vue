2bd2b0207717965999ae6d289957f8c79113c571
TRIH <istrih@icloud.com>
2025-01-18 12:27:00 +0800

    添加logo颜色的深色、浅色模式。

--- components/normal-header.vue
+++ components/normal-header.vue
@@ -5,7 +5,7 @@
   <client-only>
     <n-grid :cols="24" item-responsive>
       <n-gi offset="1" span="9 320:8 340:7 420:6 550:5 780:3" class="flex justify-center">
-        <icons-keyframe />
+        <icons-keyframe class="icon"/>
       </n-gi>
       <n-gi offset="2 " span="10 320:12">
         <n-input id="search" round placeholder="搜索更多内容">
@@ -19,4 +19,8 @@
   </client-only>
 </template>
 
-<style scoped></style>
+<style scoped>
+.icon {
+  color: var(--czjB-5);
+}
+</style>
